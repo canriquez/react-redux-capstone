73324ea6dff8475d4de0d55d87c08005aeb7821a
Carlos Anriquez <canriquez@gmail.com>
2020-09-19 20:10:55 -0300

    updates API values using this.state.mainFilter store default value

--- src/components/App.js
+++ src/components/App.js
@@ -3,32 +3,58 @@ import DashBoard from '../container/DashBoard'
 import { connect } from 'react-redux';
 import { updateApiRenderList } from '../actions/index';
 
-const App = ({
-    renderApiAssetsLists
-}) => {
+class App extends React.Component {
+    constructor (props){
+        super(props)
+        
+        const { state, renderApiAssetsLists } = props
+        this.state = state
+        this.renderApiAssetsLists = renderApiAssetsLists
+        this.renderApiAssetsLists = this.renderApiAssetsLists.bind(this);
 
-const initialConf = {
-    url:null,  
-    currency:'usd',
-    filter:'market_cap_desc',
-    results:'200',
-    page:'1'
+    
+    }
+
+//Initial conf will be the initial state of a store reducer where we will update the 
+// selected filters
+// API will be only called if currency has changed
+
+    componentDidMount() {
+
+        const initialConf = {
+            url:null,  
+            currency:'usd',
+            filter:this.state.mainFilter,
+            results:'200',
+            page:'1'
+        }
+        this.renderApiAssetsLists(initialConf);
+    }
+
+
+
+
+
+render() {
+    return (
+        <>
+            <div>
+                <h1>Crypto Catalog App</h1>
+            </div>
+            <DashBoard />
+        </>
+    )
 }
 
-renderApiAssetsLists(initialConf); //Updates root/home screen with fresh API assets values.
-return (
-    <>
-        <div>
-            <h1>Crypto Catalog App</h1>
-        </div>
-        <DashBoard />
-    </>
-)
-};
+}
+
+const mapStateToProps = state => ({
+  state,
+});
 
 const mapDispatchToProps = dispatch => ({
   renderApiAssetsLists: (conf) => dispatch(updateApiRenderList(conf)),
 });
 
 
-export default connect(null, mapDispatchToProps)(App);
+export default connect(mapStateToProps, mapDispatchToProps)(App);
