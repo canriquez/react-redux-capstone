c13348a85620cfb84f7f0dd7f576f5539ac8d73d
Carlos Anriquez <canriquez@gmail.com>
2020-09-20 18:21:20 -0300

    includes input search field. complete hint optional algorithm

--- src/container/DashBoard.js
+++ src/container/DashBoard.js
@@ -33,23 +33,61 @@ const sortAssetsList = (object, page, property) => {
     console.log('attempt sorting ..');
     const itemsPerPage = 5;
     const from = (itemsPerPage * page) - itemsPerPage;
-    const to = (itemsPerPage * page) - 1
+    const to = (itemsPerPage * page)
     return object.sort((a, b) => b[property] - a[property]).slice(from, to)
 }
 
-
-const renderDashBoard = ({
+//Using React Hooks
+const RenderDashBoard = ({
     state,
     changeMainFilter,
     changeCurrencyFilter,
     handlePaginator
 }) => {
 
+    let keysearch = [];
+    const [input, setInput] = React.useState('');
+    const [ks, setKs] = React.useState([{ name: '', idpos: null }]);
+
+    const handleKeySearch = (inputValue) => {
+        if (input.length < 2) { return [] }
+        keysearch = state.crypto.map((obj, idpos) => ({ ...obj, idpos })).filter((asset) =>
+            /* asset.name.toLowerCase().startsWith(inputValue.toLowerCase()) */
+            /* new RegExp('^' + inputValue, 'i').test(asset.name) */
+            (asset.name.substr(0, inputValue.length).toUpperCase() == inputValue.toUpperCase())
+        ).map(obj => ({ name: obj.name, idpos: obj.idpos }));
+        //limits to maximum 4 results
+        return keysearch.slice(0, (keysearch.length >= 5 ? 4 : keysearch.length));
+    }
+
+    React.useEffect(() => {
+
+    })
+
+    const handleInputSearch = (event) => {
+        setInput(event.target.value)
+        setKs(handleKeySearch(event.target.value));
+        console.log(event.target.value);
+        console.log(keysearch)
+    }
+
     const sortedAssetList = sortAssetsList(state.crypto, state.page, state.mainFilter)
 
     return (
         <>
             <div>
+                <div>
+                    <input type="text" value={input} list="keysearch" onChange={handleInputSearch} />
+                    <datalist id="keysearch">
+                        {
+                            ks.map((hint) => {
+                                return (
+                                    <option key={hint.idpos}>{hint.name}</option>
+                                )
+                            })
+                        }
+                    </datalist>
+                </div>
                 <MainFilter
                     changeMainFilter={changeMainFilter}
                 />
@@ -91,6 +129,6 @@ const renderDashBoard = ({
 const DashBoard = connect(
     mapStateToProps,
     mapDispatchToProps
-)(renderDashBoard);
+)(RenderDashBoard);
 
 export default DashBoard;
