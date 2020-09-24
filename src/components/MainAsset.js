9643bcc5929eb33368ae199aa0ee92da990478fa
Carlos Anriquez <canriquez@gmail.com>
2020-09-24 16:39:46 -0300

    fix linter warnigs

--- src/components/MainAsset.js
+++ src/components/MainAsset.js
@@ -8,26 +8,26 @@ const MainAsset = ({
   currency,
 }) => (
 
-    <div className={style.mainAsset}>
-      <div className={`${style.leftBox} flexCenter`}>
-        <img src={asset.image} alt={asset.name} />
-        <a className={style.myBrand} href="https://www.carlosanriquez.com">
-          <img src="https://img.shields.io/badge/Developed%20by-Carlos%20Anriquez-red" alt="carlos anriquez" />
-        </a>
-      </div>
-      <div className={`${style.rightBox} flexCol`}>
-        <h1>{asset.id}</h1>
-        <div className={style.marketCap}>
-          <h2>{`${textToBigCurrency(asset.market_cap)} `}</h2>
-          <span>{currency}</span>
-        </div>
-        <h2>{asset.symbol}</h2>
-        <a className={style.myRepo} href="https://github.com/canriquez/react-redux-capstone">
-          <img src="https://img.shields.io/badge/Github-Repo-green" alt="repo" />
-        </a>
-      </div>
+  <div className={style.mainAsset}>
+    <div className={`${style.leftBox} flexCenter`}>
+      <img src={asset.image} alt={asset.name} />
+      <a className={style.myBrand} href="https://www.carlosanriquez.com">
+        <img src="https://img.shields.io/badge/Developed%20by-Carlos%20Anriquez-red" alt="carlos anriquez" />
+      </a>
     </div>
-  );
+    <div className={`${style.rightBox} flexCol`}>
+      <h1>{asset.id}</h1>
+      <div className={style.marketCap}>
+        <h2>{`${textToBigCurrency(asset.market_cap)} `}</h2>
+        <span>{currency}</span>
+      </div>
+      <h2>{asset.symbol}</h2>
+      <a className={style.myRepo} href="https://github.com/canriquez/react-redux-capstone">
+        <img src="https://img.shields.io/badge/Github-Repo-green" alt="repo" />
+      </a>
+    </div>
+  </div>
+);
 
 MainAsset.propTypes = {
   asset: PropTypes.shape({
